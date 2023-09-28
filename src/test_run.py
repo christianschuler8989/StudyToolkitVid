f90de6c1c3a9fdc0ead254d40e8757e30862f28b
Christian Schuler <christianschuler8989@gmail.com>
2023-09-28 12:56:10 +0200

    New filestructure and first steps for automated testing.

---
+++ src/test_run.py
@@ -0,0 +1,23 @@
+#
+# 
+# Authors: Christian Schuler & Dominik Hauser
+################################################################################
+
+import os.path as path
+
+"""
+Pipeline test class for development and bug-fixing
+"""
+
+
+# Call "setup.bh" to build directory structure and copy examples into place
+
+
+# Test: Part 1 Media Editing
+
+# Test: Part 2 Study Setup
+
+# Test: Part 3 Statistical Analysis
+
+
+
